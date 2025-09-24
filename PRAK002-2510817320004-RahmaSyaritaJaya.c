24edfe6d89754bb866f7d3cfbecc8d3499b75458
jyaritta <rahmasyaritazaya@gmail.com>
2025-09-24 15:34:47 +0800

    Upload C File

---
+++ PRAK002-2510817320004-RahmaSyaritaJaya.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    char nama[] = "Rita";
+    printf("Selamat Pagi %s\nSelamat Siang %s\nSelamat Malam %s", nama, nama, nama);
+    return 0;
+}
---
+++ PRAK004-2510817320004-RahmaSyaritaJaya.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#     Rahma Syarita Jaya     #\n");
+    printf("#        2510817320004       #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
+
